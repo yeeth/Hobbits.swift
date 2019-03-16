3e492344f5ab2e3a0cf4f2e03c5c90c763648f34
decanus <dean.eigenmann@icloud.com>
2019-03-16 18:35:58 +0100

    read and write implementations

--- Sources/Hobbits/TCP/TCPConnection.swift
+++ Sources/Hobbits/TCP/TCPConnection.swift
@@ -2,14 +2,25 @@ import Foundation
 
 class TCPConnection {
 
-    func read(length: Int) -> [UInt8] {
+    // @todo not sure if this is the way to go yet
+    fileprivate var input: InputStream?
+    fileprivate var output: OutputStream?
 
+    func read(length: Int) -> [UInt8] {
+        if length == 0 {
+            return [UInt8]()
+        }
+
+        var buffer = [UInt8](repeating: 0, count: length)
+        input?.read(&buffer, maxLength: length)
+        return buffer
     }
 
     func write(_ response: Response) throws {
         let data = String(describing: response)
+        let bytes = data.bytes
 
-        // @todo write(data.bytes)
+        output?.write(bytes, maxLength: bytes.count)
     }
 
     func close() {
