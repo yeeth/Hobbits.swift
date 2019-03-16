9561781ed9181ce4865764e957be64970f07798a
decanus <dean.eigenmann@icloud.com>
2019-03-16 18:28:50 +0100

    rethought

--- Sources/Hobbits/TCP/TCPConnection.swift
+++ Sources/Hobbits/TCP/TCPConnection.swift
@@ -2,12 +2,14 @@ import Foundation
 
 class TCPConnection {
 
-    func read() -> Request {
+    func read(length: Int) -> [UInt8] {
 
     }
 
     func write(_ response: Response) throws {
+        let data = String(describing: response)
 
+        // @todo write(data.bytes)
     }
 
     func close() {
