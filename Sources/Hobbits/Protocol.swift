1ab1ef78c65f519ade9ef35c754c81ad700482ea
Dean Eigenmann <dean@tokenate.io>
2019-03-30 17:42:38 +0100

    hobbits/0.2 (#5)
    * updated to hobbits v0.2
    
    * Update Message.swift

---
+++ Sources/Hobbits/Protocol.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum Protocol: String {
+
+    case rpc = "RPC"
+    case gossip = "GOSSIP"
+
+}
--- Tests/HobbitsTests/XCTestManifests.swift
+++ Tests/HobbitsTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(RequestTests.allTests),
+        testCase(MessageTests.allTests),
     ]
 }
 #endif
