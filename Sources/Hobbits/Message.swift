ad3ffd55a367ef62c55abfcda932a7d83b4d9881
decanus <dean.eigenmann@icloud.com>
2019-07-10 15:07:06 -0400

    removed stuff

--- Sources/Hobbits/Message.swift
+++ Sources/Hobbits/Message.swift
@@ -2,40 +2,27 @@ import Foundation
 
 public struct Message {
 
-    let version: UInt32 // @todo Version type
+    let version: UInt32
     let proto: Protocol
     let header: [UInt8]
     let body: [UInt8]
 }
 
-extension Message: CustomStringConvertible {
+extension Message {
 
-    public var bytes {
-        
+    public var bytes: [UInt8] {
+        return [UInt8]("EWP".utf8)
     }
 }
 
 extension Message {
 
-    init(serializedData input: String) {
-        var lines = input.split(separator: "\n")
-        if lines.count == 1 {
-            lines.append("")
+    init(serializedData input: [UInt8]) {
+
+        if Array(input[0...3]) != [UInt8]("EWP".utf8) {
+            // @todo error
         }
 
-        let payload = lines[1]
-
-        var request = lines[0].split(separator: " ")
-        assert(request.count >= 7) // @todo exception handling
-
-        let headersLength = Int(request[5])!
-        let bodyLength = Int(request[6])!
-
-        version = String(request[1])
-        proto = Protocol(rawValue: String(request[2]))!
-        compression = String(request[3])
-        encoding = String(request[4])
-        headers = payload.substring(start: 0, end: headersLength).bytes
-        body = payload.substring(start: headersLength, end: bodyLength).bytes
+        // @todo
     }
 }
